447a2584b8f8df9722b258191e7e41517992c3aa
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-23 22:53:58 +0000

    Add the basic wheel implementation with non exhaustive patterns

--- app/Fibo.hs
+++ app/Fibo.hs
@@ -5,38 +5,39 @@ module Fibo where
 --               anti-clockwise
 --                    |
 --                    v
-data Wheel a = Wheel [a] [a]
---                        ^
---                        |
---                     clockwise
+data Wheel a = Wheel [a] a [a]
+--                          ^
+--                          |
+--                       clockwise
 
 
 -- | Returns the head of the wheel.
-
+-- O(1)
 readW :: Wheel a -> a
-readW = undefined
+readW (Wheel _ head _) = head
 
 -- | Move the head one place counter clockwise.
-
+-- O(1)
 goLeft :: Wheel a -> Wheel a
-goLeft = undefined
+goLeft (Wheel (x:xs) head cw) = Wheel xs x (head : cw)
 
 -- | Move the head one place clockwise.
-
+-- O(1)
 goRight :: Wheel a -> Wheel a
-goRight = undefined
+goRight (Wheel acw head (x:xs)) = Wheel (head : acw) x xs
 
--- | insert a new element into a wheel, it will be the new head
-
+-- | insert a new element into a wheel, it will be the new head and shift right
+-- O(1)
 insertW :: a -> Wheel a -> Wheel a
-insertW = undefined
+insertW x (Wheel acw head cw) = Wheel acw x (head : cw)
 
 -- | deletes the head of the wheel and returns it
-
+-- O(1)
 extractW :: Wheel a -> (a, Wheel a)
-extractW = undefined
+extractW (Wheel acw head (x:xs)) = (x, Wheel acw head xs)
 
 -- | concatenate two wheels
-
+-- O(n)
 concatW :: Wheel a -> Wheel a -> Wheel a
-concatW = undefined
+concatW (Wheel acw head cw) (Wheel acw' head' cw') =
+  Wheel ((head' : cw') ++ acw) head (cw ++ acw)
