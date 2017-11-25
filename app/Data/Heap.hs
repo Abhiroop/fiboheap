cd17f714e9775e532b35a54dbb4e9e8cfd830a50
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-11-25 22:27:46 +0000

    Add some documentation

--- app/Data/Heap.hs
+++ app/Data/Heap.hs
@@ -46,7 +46,8 @@ insertW x (Wheel acw head cw) = Wheel acw x (head : cw)
 -- | deletes the head of the wheel and returns it and makes the right most element the head
 -- Amortized O(1)
 extractW :: Wheel a -> (a, Wheel a)
-extractW EmptyWheel              = error "No elements in heap" 
+extractW EmptyWheel              = error "No elements in heap"
+extractW (Wheel [] head [])      = (head, EmptyWheel)
 extractW (Wheel acw head [])
   = (head , Wheel [] (last acw) (drop 1 (reverse acw)))
 extractW (Wheel acw head (x:xs)) = (head, Wheel acw x xs)
@@ -100,35 +101,43 @@ union (FibHeap w1@(Wheel _ (h, _, _) _)) (FibHeap w2@(Wheel _ (h', _, _) _))
   | h < h'    = FibHeap $ concatW w1 w2
   | otherwise = FibHeap $ concatW w2 w1
 
--- | Extracting the minimum element of the heap
-
 -- The trick: When we are concatenating order of w' and w'' doesn't matter because consolidation takes care of marking the min
+-- Consolidation also ensures that the root wheel doesn't contain nodes of the same degree.
+
+-- | Extracting the minimum element of the heap
+-- O(log n)
 extractMin :: Ord a => FibHeap a -> (a, FibHeap a)
 extractMin (FibHeap EmptyWheel) = error "Heap contains no elements"
 extractMin (FibHeap (Wheel [] (x, _, h) [])) = (x,h)
 extractMin (FibHeap w) = let ((x,_, (FibHeap w'')), w') = extractW w
                            in (x, consolidate $ FibHeap $ concatW w' w'')
 
+-- | Convert root wheel to DA and translate DA to wheel finally
 consolidate :: Ord a => FibHeap a -> FibHeap a
 consolidate (FibHeap w) = FibHeap $ wheelDA $ makeDA w
 
+-- | Convert  DA to wheel
 wheelDA :: Ord a => IMap.IntMap (Node a) -> Wheel (Node a)
 wheelDA m = foldr insNode EmptyWheel m
 
+-- | ensure the head is the minimum
 insNode :: Ord a => Node a -> Wheel (Node a) -> Wheel (Node a)
 insNode n EmptyWheel = insertW n EmptyWheel
 insNode n@(a,_,_) w@(Wheel _ (h,_,_) _)
   | a <= h = insertW n w
   | otherwise = goRight $ insertW n w
 
+-- | Convert wheel to DA and make sure nodes of same degree are not on the root wheel
 makeDA :: Ord a => Wheel (Node a) -> IMap.IntMap (Node a)
 makeDA EmptyWheel = IMap.empty
 makeDA w          = let (n, w') = extractW w
                      in insDA n (makeDA w')
 
+-- | Ensuring that nodes of same degree gets linked
 insDA :: Ord a => Node a -> IMap.IntMap (Node a) -> IMap.IntMap (Node a)
 insDA n@(_,d,_) m = IMap.insertWith linkDA d n m
 
+-- | Simple logic for linking 2 nodes
 linkDA :: Ord a => Node a -> Node a -> Node a
 linkDA n@(a,d,(FibHeap w)) n'@(a',d',(FibHeap w'))
   | a < a'    = (a , d+1, FibHeap $ insertW n' w)
